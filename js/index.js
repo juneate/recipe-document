888d41b0eec7601c130844e53cf27a5b3dee4435
Rocco Panacci <rocco@Roccos-MacBook-Pro.local>
2020-07-06 16:18:15 -0400

    quick test

---
+++ js/index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+// Reviews loaded from a data file?
+// 		Discuss this as the bridge from websites, to apps (not covered, of course - but let's fake it for now)
+
+// Stick the title in the top?
